abca3041a0e4438dcece59455064ee2cd14ec2aa
NickRosso <nicholasrosso@comcast.net>
2014-06-27 14:32:01 -0500

    Converted indents to spaces

--- GUI.py
+++ GUI.py
@@ -12,15 +12,15 @@ Ximg = pygame.image.load("X.png")
 Oimg = pygame.image.load("O.png")
 
 def template(DISPLAY, board, Ximg, Oimg, coords):
-	DISPLAY.fill(TEAL)
-	pygame.draw.line(DISPLAY, BLACK, (100, 0),(100, 300), 2)
-	pygame.draw.line(DISPLAY, BLACK, (200, 0),(200, 300), 2)
-	pygame.draw.line(DISPLAY, BLACK, (0, 100),(300, 100), 2)
-	pygame.draw.line(DISPLAY, BLACK, (0, 200),(300, 200), 2)
+    DISPLAY.fill(TEAL)
+    pygame.draw.line(DISPLAY, BLACK, (100, 0),(100, 300), 2)
+    pygame.draw.line(DISPLAY, BLACK, (200, 0),(200, 300), 2)
+    pygame.draw.line(DISPLAY, BLACK, (0, 100),(300, 100), 2)
+    pygame.draw.line(DISPLAY, BLACK, (0, 200),(300, 200), 2)
 
-	for n in range(0,9):
-		if board[n] == X:
-			DISPLAY.blit(Ximg, (coords[n]))
+    for n in range(0,9):
+        if board[n] == X:
+            DISPLAY.blit(Ximg, (coords[n]))
 
-		if board[n] == O:
-			DISPLAY.blit(Oimg, (coords[n]))
+        if board[n] == O:
+            DISPLAY.blit(Oimg, (coords[n]))
